5cf2a3302720fd3fff07fb6ab1131b0553555480
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-07-26 22:35:31 +0530

    Initialize

---
+++ src/main/java/com/mms_backend/MmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mms_backend/MmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
