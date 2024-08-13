ee71ec9e5ddb1df9abcdcbdcc54cadc5bc0143aa
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-08-13 22:51:53 +0530

    Update Course.java

--- src/main/java/com/mms_backend/entity/AR/Course.java
+++ src/main/java/com/mms_backend/entity/AR/Course.java
@@ -18,7 +18,6 @@ public class Course {
     private String course_name;
     private int hours;
     private String type;
-    private double credit;
     private String department_id;
     private int level;
     private int semester;
