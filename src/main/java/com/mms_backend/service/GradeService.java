7d7a4ea212321313f1a7df9b7a2875e23b575f69
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-09-01 19:43:15 +0530

    Update GradeService.java

--- src/main/java/com/mms_backend/service/GradeService.java
+++ src/main/java/com/mms_backend/service/GradeService.java
@@ -2,11 +2,10 @@ package com.mms_backend.service;
 
 import com.mms_backend.entity.AR.Grade;
 import com.mms_backend.entity.Calculations;
+import com.mms_backend.entity.MarksEntity;
 import com.mms_backend.entity.StudentRegCourses;
-import com.mms_backend.repository.CalculationsRepo;
-import com.mms_backend.repository.GradeRepo;
-import com.mms_backend.repository.StudentRegCoursesRepo;
-import com.mms_backend.repository.StudentRepo;
+import com.mms_backend.repository.*;
+import com.mms_backend.repository.AR.ArMarksRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +21,10 @@ public class GradeService {
     private StudentRegCoursesRepo studentRegCoursesRepo;
     @Autowired
     private CalculationsRepo calculationsRepo;
+    @Autowired
+    private MarksRepo marksRepo;
+    @Autowired
+    private ArMarksRepo arMarksRepo;
 
     public static String reduceYear(String years){
 
@@ -277,7 +280,70 @@ public class GradeService {
                         }
 
                     }else if (grade.getGrade().equals("MC")){
-                        
+                        List<MarksEntity> preMidMarksList =    arMarksRepo.getSelectedStudentSelectedExamMarksBySelectedCourseAndSelectedAcademicYear(student.getStudent_id(),course_id,pre_academic_year,"Mid");
+                        List<MarksEntity> preEndMarksList =    arMarksRepo.getSelectedStudentSelectedExamMarksBySelectedCourseAndSelectedAcademicYear(student.getStudent_id(),course_id,pre_academic_year,"End");
+
+                        boolean isMidMc =false;
+                        boolean isEndMc = false;
+
+                        if(!preMidMarksList.isEmpty()){            //Check is there any previous MC Mid exams
+                            for(MarksEntity preMidMarks : preMidMarksList){
+                                if(preMidMarks.getAssignment_score().equals("MC")){
+                                    isEndMc = true;
+                                }
+                            }
+                        }
+
+                        if(!preEndMarksList.isEmpty()){             //Check is there any previous MC End exams
+                            for(MarksEntity preEndMarks : preEndMarksList){
+                                if(preEndMarks.getAssignment_score().equals("MC")){
+                                    isMidMc = true;
+                                }
+                            }
+                        }
+
+                        if(isMidMc && isEndMc){         //If both exams are MC
+                            List<Calculations> endCalculationList = calculationsRepo.getCalculationData(student.getStudent_id(),course_id,academic_year,"End");
+                            List<Calculations> ca_CalculationList = calculationsRepo.getCalculationData(student.getStudent_id(),course_id,academic_year,"CA");
+                            List<Calculations> mid_CalculationList = calculationsRepo.getCalculationData(student.getStudent_id(),course_id,academic_year,"Mid");
+
+                            double sum = 0.0 ;
+                            double roundedSum = 0.0;
+
+                            if(grade.getCa_eligibility().equals("Eligible")){
+
+                                if(!endCalculationList.isEmpty()){
+
+                                    for(Calculations endCalculation : endCalculationList){
+                                        sum = sum + Double.parseDouble(endCalculation.getPercentage());
+                                    }
+                                }
+
+                                if(!ca_CalculationList.isEmpty()){
+                                    for(Calculations ca_Calculation : ca_CalculationList){
+                                        sum = sum + Double.parseDouble(ca_Calculation.getPercentage());
+                                    }
+                                }
+
+                                if(!mid_CalculationList.isEmpty()){
+                                    for(Calculations mid_Calculation : mid_CalculationList){
+                                        sum = sum + Double.parseDouble(mid_Calculation.getPercentage());
+                                    }
+                                }
+
+                                roundedSum = Math.round(sum*100)/100.00 ;
+
+                                grade.setTotal_final_mark(String.valueOf(sum));
+                                grade.setTotal_rounded_mark(String.valueOf(roundedSum));
+
+                                gradeRepo.save(grade);
+
+                            }
+                        }
+
+
+
+
                     }
                 }
             }
